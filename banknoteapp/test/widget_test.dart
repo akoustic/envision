fbb79a4b9fcdda0be1f61c121fa019b09521dc7b
mayhem-am <amakanksh@gmail.com>
2020-05-23 20:12:24 +0530

    Update widget_test.dart

--- banknoteapp/test/widget_test.dart
+++ banknoteapp/test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:cricketshotclassifier/main.dart';
+import 'package:banknoteapp/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
